66188dac4b498e9498064bf731ff309d6a0526b8
mdsdsardar <83182506+mdsdsardar@users.noreply.github.com>
2024-08-10 13:10:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
-
