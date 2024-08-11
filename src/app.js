06d8c2d89c54ea20f1a9aebcb3b1e173537d5776
mdsdsardar <83182506+mdsdsardar@users.noreply.github.com>
2024-08-11 18:53:38 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hi MOM\n');
 });
 
 server.listen(3000, '0.0.0.0', () => {
